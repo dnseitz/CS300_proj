703f7fe1fe8ca055b18e0e05d71b2052956d439c
Daniel Seitz <Daniel Seitz>
2015-11-26 13:05:12 -0800

    Added local networking test

---
+++ LocalNetworkTest/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS300Net;
+
+namespace LocalNetworkTest
+{
+    class Client : NetObserver
+    {
+        public static void Main(string[] args)
+        {
+            NetworkManager netMan = new NetworkManager();
+            Client client = new Client();
+
+            netMan.Register(client);
+
+            Console.Write("Enter ip to connect to: ");
+            string ipAddr = Console.ReadLine();
+
+            if (!netMan.Connect(ipAddr))
+            {
+                Console.WriteLine("Failed to connect to {0}", ipAddr);
+                netMan.Disconnect();
+                return;
+            }
+
+            string msg = null;
+
+            do
+            {
+                Console.Write("Enter message to send to server: ");
+                msg = Console.ReadLine();
+                netMan.Send(ipAddr, Encoding.ASCII.GetBytes(msg));
+            } while (msg.Equals("exit"));
+
+            netMan.Disconnect();
+        }
+
+        public void DataRecieved(byte[] data)
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(data));
+        }
+    }
+}
---
+++ LocalNetworkTest/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS300Net;
+
+namespace LocalNetworkTest
+{
+    class Server : NetObserver
+    {
+        static void Main(string[] args)
+        {
+            NetworkManager netMan = new NetworkManager();
+            Server server = new Server();
+
+            netMan.Register(server);
+
+            Console.WriteLine(NetworkManager.LocalIP.ToString());
+
+            netMan.Listen();
+
+            Console.Read();
+
+            netMan.StopListen();
+            netMan.Disconnect();
+        }
+
+        public void DataRecieved(byte[] data)
+        {
+            Console.WriteLine(Encoding.ASCII.GetString(data));
+        }
+    }
+}
